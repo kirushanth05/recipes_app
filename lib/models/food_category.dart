2ebf38ecdeef4cd88663c60d00f25ef3fd870f7b
kirushanth05 <kirushanthvijay@gmail.com>
2025-02-05 21:59:54 +0530

    food category model added.

---
+++ lib/models/food_category.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:recipe_app/models/food.dart';
+
+class FoodCategory {
+  final String title;
+
+  /// color for the widget in the home screen
+  Color color;
+
+  /// avalable food under this category.
+  List<Food>? availableRecipe = [];
+
+  FoodCategory(
+      {required this.title, this.color = Colors.green, this.availableRecipe});
+}
