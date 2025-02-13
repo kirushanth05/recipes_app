57b4a9cb5095defcd84a3d9d1930fd29fb8a9f31
kirushanth05 <kirushanthvijay@gmail.com>
2025-02-13 21:24:39 +0530

    session 24

--- lib/routes/router_functions.dart
+++ lib/routes/router_functions.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/models/food.dart';
 import 'package:recipe_app/models/food_category.dart';
+import 'package:recipe_app/screens/favorites_screen.dart';
 import 'package:recipe_app/screens/food_recipes_screen.dart';
+import 'package:recipe_app/screens/recipe_details.dart';
 
-navigateToFoodRecipiesScreen(BuildContext context, FoodCategory category) {
+void navigateToFoodRecipiesScreen(BuildContext context, FoodCategory category) {
   Navigator.push(
     context,
     MaterialPageRoute(
       builder: (ctx) => FoodRecipesScreen(category),
+    ),
+  );
+}
+
+void navigateToFavorites(BuildContext context) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => const FavoritesScreen(),
+    ),
+  );
+}
+
+void navigateToRecipe(BuildContext context, Food recipe) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => RecipeDetails(recipe: recipe),
     ),
   );
 }
